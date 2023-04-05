85fac100483b9dc509bf1de35532e7c0af6676cf
yellowsink <yellowsink@riseup.net>
2023-04-05 21:06:56 +0100

    add colour customization

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -6,10 +6,16 @@ const App: Component = () => {
   const [active, setActive] = createSignal<string>();
 
   return (
-    <>
+    <div style="background: black; color: white">
       <h1>Tz Picker test - you have picked {active()}</h1>
-      <Picker active={active()} onChoose={setActive} />
-    </>
+      <Picker
+        active={active()}
+        onChoose={setActive}
+        col="#F00"
+        colHov="#0F0"
+        colActive="#00F"
+      />
+    </div>
   );
 };
 
