3a27cb47584c1e99ea72ee7d05417be4bad07614
yellowsink <yellowsink@riseup.net>
2023-04-05 20:53:21 +0100

    working picker

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -1,12 +1,14 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 
 import Picker from "@uwu/tz-picker";
 
 const App: Component = () => {
+  const [active, setActive] = createSignal<string>();
+
   return (
     <>
-      <h1>Tz Picker test</h1>
-      <Picker />
+      <h1>Tz Picker test - you have picked {active()}</h1>
+      <Picker active={active()} onChoose={setActive} />
     </>
   );
 };
