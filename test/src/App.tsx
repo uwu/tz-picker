fbf5d08ea8ed14838f91bc9a2957a099180e561a
yellowsink <yellowsink@riseup.net>
2023-04-05 21:20:43 +0100

    oops! all tabs!

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -3,20 +3,20 @@ import { Component, createSignal } from 'solid-js';
 import Picker from "@uwu/tz-picker";
 
 const App: Component = () => {
-  const [active, setActive] = createSignal<string>();
+	const [active, setActive] = createSignal<string>();
 
-  return (
-    <div style="background: black; color: white">
-      <h1>Tz Picker test - you have picked {active()}</h1>
-      <Picker
-        active={active()}
-        onChoose={setActive}
-        col="#F00"
-        colHov="#0F0"
-        colActive="#00F"
-      />
-    </div>
-  );
+	return (
+		<div style="background: black; color: white">
+			<h1>Tz Picker test - you have picked {active()}</h1>
+			<Picker
+				active={active()}
+				onChoose={setActive}
+				col="#F00"
+				colHov="#0F0"
+				colActive="#00F"
+			/>
+		</div>
+	);
 };
 
 export default App;
